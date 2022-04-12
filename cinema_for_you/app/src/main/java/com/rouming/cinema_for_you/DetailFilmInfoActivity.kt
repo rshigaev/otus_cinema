a3a648044d77fab0f7035c8636ef7ba01e21d7d5
rshigaev <roman_shigaev@mail.ru>
2022-04-12 22:41:28 +0300

    накидал UI

---
+++ cinema_for_you/app/src/main/java/com/rouming/cinema_for_you/DetailFilmInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.rouming.cinema_for_you
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailFilmInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_film_info)
+    }
+}
--- cinema_for_you/app/src/main/java/com/rouming/cinema_for_you/MainActivity.kt
+++ cinema_for_you/app/src/main/java/com/rouming/cinema_for_you/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var filmNames = listOf("Форсаж 5","Железеый человек","Маска","Росомаха","Шерлок Холмс")
+        var filmImages = listOf("Форсаж 5","Железеый человек","Маска","Росомаха","Шерлок Холмс")
+        var filmDescriptions = listOf("Форсаж 5","Железеый человек","Маска","Росомаха","Шерлок Холмс")
+
+
     }
 }
