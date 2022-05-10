b321d5d82c942e354214dd06b70002e28d025fb5
rshigaev <68125445+rshigaev@users.noreply.github.com>
2022-05-10 21:42:32 +0300

    отработка замечаний по п.3 и 5

--- cinema_for_you/app/src/main/java/com/rouming/cinema_for_you/FilmDiffUtils.kt
+++ cinema_for_you/app/src/main/java/com/rouming/cinema_for_you/FilmDiffUtils.kt
@@ -3,21 +3,19 @@ package com.rouming.cinema_for_you
 import android.util.Log
 import androidx.recyclerview.widget.DiffUtil
 
-class FilmDiffUtils(var oldList: MutableList<FilmItem>, var newList: MutableList<FilmItem>):DiffUtil.Callback() {
+class FilmDiffUtils(private val oldList: MutableList<FilmItem>, private val newList: MutableList<FilmItem>):DiffUtil.Callback() {
 
     override fun getOldListSize(): Int = oldList.size
 
     override fun getNewListSize(): Int = newList.size
 
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition] == newList[newItemPosition]
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+    }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val oldItem = oldList[oldItemPosition]
         val newItem = newList[newItemPosition]
-        if(oldItem != newItem){
-            Log.d("OTUS","oldItem = $oldItem")
-            Log.d("OTUS","newItem = $newItem")
-        }
-        return oldItem.isTouched == newItem.isTouched && oldItem.like == newItem.like
+        return oldItem.like == newItem.like && oldItem.isTouched == newItem.isTouched
     }
 }
